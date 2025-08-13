d3b48b1c6fce1195c72cc21f40dd3a734fe75fe0
Daniel Vollbro <wollbro90@gmail.com>
2025-08-13 23:45:23 +0200

    Continue the work with setting up vault

--- roles/vault/files/restore_vault.sh
+++ roles/vault/files/restore_vault.sh
@@ -4,8 +4,6 @@ set -e
 LOG_FILE="/var/log/restore_vault.log"
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
-export VAULT_ADDR=http://127.0.0.1:8200
-
 if [ -f /mnt/nfs/vault_backups/latest.snap ]; then
   vault operator raft snapshot restore /mnt/nfs/vault_backups/latest.snap
   echo "[$TIMESTAMP] Backup restored" > $LOG_FILE
